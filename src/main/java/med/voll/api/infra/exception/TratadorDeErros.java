b388f4b0cfb76a38c98908c290144d798415d4b5
Iagosilvabrito <iago.silvabrito1@outlook.com>
2025-04-05 13:46:19 -0300

    Primeiro commit

---
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity TratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity TratarError400(MethodArgumentNotValidException ex){
+        var error = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(error.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensage){
+        public DadosErroValidacao(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+}
