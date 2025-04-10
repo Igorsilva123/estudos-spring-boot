bd6e401883eb96068f9136e1ecae345a18184eaa
IgorSilva123 <igor.silvabrito@outlook.com>
2025-04-10 14:56:07 -0300

    spring boot project

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consulta;
 
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
@@ -20,19 +22,31 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsultas dados){
 
-        var medico = medicoRepository.findById(dados.idMedico()).get();
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+
+        if(!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("id do paciente não existe!!");
+        }
+
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Id do medico não exite!");
+        }
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var medico = escolherMedico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultasRepository.save(consulta);
 
     }
 
+    private Medico escolherMedico(DadosAgendamentoConsultas dados) {
+        if(dados.idMedico() != null ){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
 
-
-
-
-
-
+        if(dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade do medico é obrigatoria quando o medico não for escolhido!");
+        }
+        return medicoRepository.escolherMedicoAleatorioLivreData(dados.especialidade(), dados.data());
+    }
 
 
 
